b376586186793812ee57f3442c94de4dde67394d
Leslie Musengi <leslie.musengi@icloud.com>
2020-07-11 15:28:49 +0200

    created new .py script for if statements

--- mathsFunctions.py
+++ mathsFunctions.py
@@ -1,7 +1,12 @@
+import math
 import os
 
 os.system("clear")
 
+# we can access any math methods using the . operator eg math.log10(100)
 x = 2.6
 print(round(x))  # rounds a number up or down based on 0.5
-
+a = -3.9
+print(abs(a))  # always returns the positive form of a number
+print(math.ceil(9.2))  # always rounds up
+print(math.floor(9.9))  # always rounds down
